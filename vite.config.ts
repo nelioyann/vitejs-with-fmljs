1e2b88ab8b1260fbf50bff89f2b529169ab772d8
nelioyann <nanayannick1998@gmail.com>
2024-10-02 15:18:14 +0200

    fix: resolve ~antd alias - enables JavaScript in Less which is necessary for Ant Design

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~antd": path.resolve(__dirname, "node_modules/antd"),
+    },
+  },
+  // https://stackoverflow.com/a/75970193
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
